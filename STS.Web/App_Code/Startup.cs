834bd397a4a4861faf18f50941d1924e5b72a244
asalhani <asalhani@outlook.com>
2018-01-28 20:08:10 +0300

    upload web project

---
+++ STS.Web/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(STS.Web.Startup))]
+namespace STS.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
