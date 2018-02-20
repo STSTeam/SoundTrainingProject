ba61ca04aadaaf1505946c85bb86967186674dec
Adib Ahmad <asalhani@outlook.com>
2018-02-20 22:39:24 +0300

    update some links and text

--- STS.WebApp.V2/STS.FrontEnd/src/app/patient/patient-routing.module.ts
+++ STS.WebApp.V2/STS.FrontEnd/src/app/patient/patient-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
-    path: '',  component: LayoutComponent, children: [
+    path: '',  component: LayoutComponent, canActivate: [AuthGuard], children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home',  component: HomeComponent },
 
--- STS.WebApp.V2/STS.FrontEnd/src/app/public/login/login.component.ts
+++ STS.WebApp.V2/STS.FrontEnd/src/app/public/login/login.component.ts
@@ -30,7 +30,6 @@ export class LoginComponent implements OnInit {
 
   login() {
       this.loading = true;
-      debugger;
       this.authenticationService.login(this.model.username, this.model.password)
           .subscribe(
               data => {
