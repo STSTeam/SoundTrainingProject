799bda92facac8fa9a6705746dd884063e9e5484
Adib Ahmad <asalhani@outlook.com>
2018-02-23 13:14:33 +0300

    update db

---
+++ STS.WebApp.V2/STS.DataBase/dbo/Tables/Module.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Module] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Order]       INT            NOT NULL,
+    [Title]       NVARCHAR (150) NOT NULL,
+    [Descreption] NVARCHAR (250) NULL,
+    [ImageName]   NVARCHAR (150) NULL,
+    CONSTRAINT [PK_Module] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
