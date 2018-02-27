b032159350ee8bb23cd9f0f872fbbaa31c384eed
Adib Ahmad <asalhani@outlook.com>
2018-02-27 06:16:58 +0300

    update audio

--- STS.WebApp.V2/STS.FrontEnd/src/app/_directives/training-item-view/training-item-view.component.ts
+++ STS.WebApp.V2/STS.FrontEnd/src/app/_directives/training-item-view/training-item-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges, ViewChild  } from '@angular/core';
 import { SoundModel } from "../../_models/sound.model";
 
 @Component({
@@ -6,14 +6,24 @@ import { SoundModel } from "../../_models/sound.model";
   templateUrl: './training-item-view.component.html',
   styleUrls: ['./training-item-view.component.css']
 })
-export class TrainingItemViewComponent implements OnInit {
+export class TrainingItemViewComponent implements OnInit, OnChanges  {
+
+  @ViewChild('soundCtr') soundCtr: any;
+
+  ngOnChanges(changes: SimpleChanges): void {
+  let xx = this.soundCtr.nativeElement;
+  xx.src = "./assets/_support_files/MP3/" + changes.sound.currentValue.name + ".mp3";
+
+  let t = changes.sound.currentValue;
+  this.sound.name = t.name;
+  }
 
   constructor() { }
 
-  @Input()sound:SoundModel; 
+  @Input()sound:SoundModel;
   
   ngOnInit() {
-    
+
   }
 
 }
