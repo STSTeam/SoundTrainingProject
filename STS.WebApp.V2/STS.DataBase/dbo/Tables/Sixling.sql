c6b76156a008b49310b0b802222cbda095f19afa
Adib Ahmad <asalhani@outlook.com>
2018-06-08 08:11:10 +0300

    delete

--- STS.WebApp.V2/STS.DataBase/dbo/Tables/Sixling.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Sixling] (
-    [Id]          INT            NOT NULL,
-    [Title]       NVARCHAR (500) NOT NULL,
-    [Descreption] NVARCHAR (500) NULL,
-    [SoundName]   NVARCHAR (500) NOT NULL,
-    [ImageName]   NVARCHAR (500) NOT NULL,
-    CONSTRAINT [PK_Sixling] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- STS.WebApp.V2/STS.DataBase/dbo/Tables/UserCompletedModules.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UserCompletedModules] (
-    [UserId]      INT      NOT NULL,
-    [ModuleId]    INT      NOT NULL,
-    [CreatedDate] DATETIME CONSTRAINT [DF_UserCompletedModules_CreatedDate] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [FK_UserCompletedModules_Module] FOREIGN KEY ([ModuleId]) REFERENCES [dbo].[Module] ([Id]),
-    CONSTRAINT [FK_UserCompletedModules_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
-    CONSTRAINT [IX_UserCompletedModules] UNIQUE NONCLUSTERED ([UserId] ASC, [ModuleId] ASC)
-);
-
