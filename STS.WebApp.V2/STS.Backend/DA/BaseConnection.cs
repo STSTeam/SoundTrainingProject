ced340e2d027431f2b0c7db196fdfea50f69d8c9
Adib <asalhani@outlook.com>
2018-08-18 23:51:57 +0300

    update connection string

--- STS.WebApp.V2/STS.Backend/DA/BaseConnection.cs
+++ STS.WebApp.V2/STS.Backend/DA/BaseConnection.cs
@@ -13,12 +13,12 @@ namespace WebApi.DA
         {
             var connectionString = "";
 
-            connectionString = @"Server=tcp:amwajrehabwebappdbserver.database.windows.net,1433;Initial Catalog=STSDb;Persist Security Info=False;User ID=asalhani;Password=@Sarahzidane05@{your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            connectionString = @"Server=tcp:amwajrehabwebappdbserver.database.windows.net,1433;Initial Catalog=STSDb;Persist Security Info=False;User ID=asalhani;Password=@Sarahzidane05@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-#if DEBUG
- connectionString =
-            @"Data Source=localhost\sqlexpress;Initial Catalog=STSDb;User ID=sa;Password=P@ssw0rd;";
-#endif
+//#if DEBUG
+// connectionString =
+//            @"Data Source=localhost\sqlexpress;Initial Catalog=STSDb;User ID=sa;Password=P@ssw0rd;";
+//#endif
 
             dbConnection = new System.Data.SqlClient.SqlConnection(connectionString);
         }
